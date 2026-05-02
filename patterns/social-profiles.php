9f23f43df3ef76dedb7d472e81631e3b0270bc37
Chandra Patel <chandrapatel@users.noreply.github.com>
2026-05-02 21:13:21 +0530

    Update header template part and single template for personal blog

---
+++ patterns/social-profiles.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Social Profiles
+ * Slug: lumio/social-profiles
+ * Categories: 
+ */
+?>
+<!-- wp:social-links {"iconColor":"black","iconColorValue":"#000000","openInNewTab":true,"className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"left","orientation":"horizontal","flexWrap":"wrap"},"style":{"spacing":{"blockGap":{"top":"20px","left":"20px"}}}} -->
+<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"https://profiles.wordpress.org/chandrapatel/","service":"wordpress"} /-->
+
+<!-- wp:social-link {"url":"https://github.com/chandrapatel","service":"github"} /-->
+
+<!-- wp:social-link {"url":"https://twitter.com/chandrappatel","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"https://www.linkedin.com/in/chandra287","service":"linkedin"} /--></ul>
+<!-- /wp:social-links -->
